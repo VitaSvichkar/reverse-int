adfc8c4e4a6238bb68b25dfa36b45fc51698c270
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-08-14 22:49:25 +0300

    solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const strArr = String(Math.abs(n)).split("");
+    const reverseNum = parseInt(strArr.reverse().join(""));
+    return reverseNum;
+};
